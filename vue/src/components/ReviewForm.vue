03d8290476caa0148fd370af5a840ef63c080c72
Jeff Bee <jeffreypbee@gmail.com>
2023-08-10 16:01:00 -0400

    Working on add review front end

--- vue/src/components/ReviewForm.vue
+++ vue/src/components/ReviewForm.vue
@@ -2,15 +2,17 @@
   <div>
       <form action="#" id="reviewform" @submit.prevent="review ? updateReview() : addReview()">
           <label for="reviewtitle">Review Title: </label>
-          <input type="text" name="reviewtitle" id="reviewtitle"  v-model="newReview.title">
+          <input type="text" name="reviewtitle" id="reviewtitle"  v-model="newReview.reviewTitle">
           <label for="reviewtext">Review Text: </label>
-          <textarea name="reviewtext" id="reviewtext" cols="30" rows="10" v-model="newReview.text"></textarea>
+          <textarea name="reviewtext" id="reviewtext" cols="30" rows="10" v-model="newReview.reviewText"></textarea>
           <input type="submit">
       </form>
   </div>
 </template>
 
 <script>
+import VideoGameService from '@/services/videogameService.js'
+
 export default {
     data() {
         return {
@@ -22,10 +24,13 @@ export default {
         if (this.review) {
             this.newReview = this.review;
         }
+        this.newReview.gameId = this.$route.params.id;
     },
     methods: {
         addReview() {
-            
+            VideoGameService.addGameReview(this.review).then(response => {
+                console.log(response);
+            })
         },
         updateReview() {
             
