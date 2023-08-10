a8f3ceb51264d737a81f34a451aa90167b81b686
Jeff Bee <jeffreypbee@gmail.com>
2023-08-10 13:24:59 -0400

    Added animated background

--- vue/src/components/ReviewCard.vue
+++ vue/src/components/ReviewCard.vue
@@ -1,17 +1,55 @@
 <template>
   <div class="videogamereview">
-      <h4>{{review.title}}</h4>
+      <div class="review-titlearea">
+          <h4>{{review.title}}</h4>
+          <div class="review-edit-delete">
+            <button @click.prevent="editReview = !editReview">Edit</button>
+            <button>Delete</button>
+        </div>
+      </div>
+      
       <p>{{review.text}}</p>
+      <div class="review-username">--UserName</div>
+      <ReviewForm v-if="editReview" :review="review" />
   </div>
 </template>
 
 <script>
+import ReviewForm from '@/components/ReviewForm.vue'
 export default {
+    components: {
+        ReviewForm
+    },
     name: 'review-card',
-    props: ['review']
+    props: ['review'],
+    data() {
+        return {
+            editReview: false
+        }
+    }
 }
 </script>
 
 <style>
-
+.videogamereview {
+    border-radius: 20px;
+    background-color: rgba(119,136,153, 1);
+    padding: 10px;
+    margin-top: 10px;
+    display: flex;
+    flex-direction: column;
+}
+.videogamereview h4 {
+    margin-top: 5px;
+    margin-bottom: 0px;
+}
+.review-titlearea {
+    display: flex;    
+    border-bottom: 1px solid white;
+    align-items: flex-end;
+    justify-content: space-between;
+}
+.review-username {
+    align-self: flex-end;
+}
 </style>
