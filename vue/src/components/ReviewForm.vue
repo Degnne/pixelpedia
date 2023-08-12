b8c6052bd2d9bc8d25a126350d30d2b17f4ee2ac
Jeff Bee <jeffreypbee@gmail.com>
2023-08-12 15:49:44 -0400

    Pulling

--- vue/src/components/ReviewForm.vue
+++ vue/src/components/ReviewForm.vue
@@ -1,47 +1,54 @@
 <template>
   <div>
-      <form action="#" id="reviewform" @submit.prevent="review ? updateReview() : addRating()">
-          <div>
-          <table>
-              <tr>
-                  <td>Story:</td>
-                  <td><input type="range" min="0" max="10" v-model="newRating.storyRating" /></td>
-                  <td>{{newRating.storyRating}}</td>
-              </tr>
-              <tr>
-                  <td>Visual:</td>
-                  <td><input type="range" min="0" max="10" v-model="newRating.visualRating" /></td>
-                  <td>{{newRating.visualRating}}</td>
-              </tr>
-              <tr>
-                  <td>Audio:</td>
-                  <td><input type="range" min="0" max="10" v-model="newRating.audioRating" /></td>
-                  <td>{{newRating.audioRating}}</td>
-              </tr>
-              <tr>
-                  <td>Gameplay:</td>
-                  <td><input type="range" min="0" max="10" v-model="newRating.gameplayRating" /></td>
-                  <td>{{newRating.gameplayRating}}</td>
-              </tr>
-              <tr>
-                  <td>Difficulty:</td>
-                  <td><input type="range" min="0" max="10" v-model="newRating.difficultyRating" /></td>
-                  <td>{{newRating.difficultyRating}}</td>
-              </tr>
-              <tr>
-                  <td>Overall:</td>
-                  <td><input type="range" min="0" max="10" v-model="newRating.overallRating" /></td>
-                  <td>{{newRating.overallRating}}</td>
-              </tr>
-          </table>         
-  </div>
-        <button @click.prevent="plusReview = !plusReview">Add a Review</button>
-        <div v-if="plusReview">
-          <label for="reviewtitle">Review Title: </label>
-          <input type="text" name="reviewtitle" id="reviewtitle"  v-model="newReview.reviewTitle" required />
-          <label for="reviewtext">Review Text: </label>
-          <textarea name="reviewtext" id="reviewtext" cols="30" rows="10" v-model="newReview.reviewText"></textarea>
-          </div>
+      <form action="#" class="reviewform" @submit.prevent="review ? updateReview() : addRating()">
+          <div class="rating-and-review">
+              <div>
+              <h4>Rating</h4>
+            <table>
+                <tr>
+                    <td>Story:</td>
+                    <td><input type="range" min="0" max="10" v-model="newRating.storyRating" /></td>
+                    <td>{{newRating.storyRating}}</td>
+                </tr>
+                <tr>
+                    <td>Visual:</td>
+                    <td><input type="range" min="0" max="10" v-model="newRating.visualRating" /></td>
+                    <td>{{newRating.visualRating}}</td>
+                </tr>
+                <tr>
+                    <td>Audio:</td>
+                    <td><input type="range" min="0" max="10" v-model="newRating.audioRating" /></td>
+                    <td>{{newRating.audioRating}}</td>
+                </tr>
+                <tr>
+                    <td>Gameplay:</td>
+                    <td><input type="range" min="0" max="10" v-model="newRating.gameplayRating" /></td>
+                    <td>{{newRating.gameplayRating}}</td>
+                </tr>
+                <tr>
+                    <td>Difficulty:</td>
+                    <td><input type="range" min="0" max="10" v-model="newRating.difficultyRating" /></td>
+                    <td>{{newRating.difficultyRating}}</td>
+                </tr>
+                <tr>
+                    <td>Overall:</td>
+                    <td><input type="range" min="0" max="10" v-model="newRating.overallRating" /></td>
+                    <td>{{newRating.overallRating}}</td>
+                </tr>
+            </table>
+            <button @click.prevent="plusReview = !plusReview">Add a Review</button>
+            </div>
+            
+            <div v-if="plusReview" class="plusreview">
+                <h4>Review</h4>
+                <label for="reviewtitle">Review Title: </label>
+                <input type="text" name="reviewtitle" id="reviewtitle"  v-model="newReview.reviewTitle" required />
+                <label for="reviewtext">Review Text: </label>
+                <textarea name="reviewtext" id="reviewtext" cols="30" rows="6" v-model="newReview.reviewText"></textarea>
+            </div>    
+        </div>
+        
+        
           <input type="submit">
       </form>
   </div>
@@ -101,11 +108,20 @@ export default {
 </script>
 
 <style>
-#reviewform {
+.reviewform {
     display: flex;
     flex-direction: column;
 }
-#reviewform label {
+.reviewform label {
     margin-top: 10px;
+}
+.rating-and-review {
+    display: flex;
+    justify-content: space-around;
+}
+
+.plusreview {
+    display: flex;
+    flex-direction: column;
 }
 </style>
